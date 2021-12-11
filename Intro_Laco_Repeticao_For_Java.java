4972bd71e548774016112635bcadec83dc130554
Natalia <nataliahisf@gmail.com>
2021-12-10 21:09:47 -0300

    Acrescentado intro ao laco de repeticao

---
+++ Intro_Laco_Repeticao_For_Java.java
@@ -0,0 +1,22 @@
+package JavaBlocoI;
+
+public class Intro_Laco_Repeticao_For_Java
+	{
+
+	public static void main(String[] args)
+	
+	{
+		// TODO Auto-generated method stub
+
+		
+		int x,soma=0;
+		
+		for(x=1;x<=10;x++)
+		{
+			System.out.printf("\n %d",x);
+			soma = soma + x;
+		}
+		System.out.printf("\nSoma total %d",soma);
+	}
+
+}
---
+++ Intro_Laco_repeticao_While_Java.java
@@ -0,0 +1,34 @@
+package JavaBlocoI;
+
+import java.util.Scanner;
+
+public class Intro_Laco_repeticao_While_Java
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+		Scanner entrada = new Scanner (System.in);
+		
+		int idade;
+		
+		System.out.printf("Entre com a sua idade: ");
+		idade = entrada.nextInt();
+		
+		while(idade>=1)
+		{
+			System.out.printf("\nSua idade: %d",idade);
+			if (idade>=18)
+			{
+				System.out.printf("\nVocê é maior de idade.");
+			}
+			else
+			{
+				System.out.printf("Você é menor de idade.");
+			}
+		System.out.printf("\nEntre com a sua idade: ");
+		idade = entrada.nextInt();
+		}
+	}
+}
